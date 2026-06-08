2246dff39bca55b6f4ef3551b13f24bb0a2acd46
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2026-06-08 12:56:37 -0300

    Add transaction types for creating, updating, and filtering transactions

---
+++ backend/src/types/transaction.types.ts
@@ -0,0 +1,35 @@
+
+export interface CreateTransactionDTO {
+    type:'RECEITA' | 'DESPESA'
+    amount : number
+    paymentMethod: 'PIX'| 'CREDITO' | 'DEBITO' | 'DINHEIRO' | 'CONVENIO'
+    description?: string
+    category?:string
+    appointmentId?:string
+    paidAt?:string
+
+}
+
+export interface UpdateTransactionDTO {
+    amount?: number
+    paymentMethod?: 'PIX'| 'CREDITO' | 'DEBITO' | 'DINHEIRO' | 'CONVENIO'
+    description?: string
+    category?:string
+    paidAt?:string
+
+}
+
+export interface TransactionFiltersDTO {
+    type?: 'RECEITA' | 'DESPESA'
+    paymentMethod?: string
+    category?:string
+    startDate?: string
+    endDate?: string
+    page?:number
+    limit?:number
+}
+
+export interface transactionReportDTO {
+    startDate:string
+    endDate: string
+}
