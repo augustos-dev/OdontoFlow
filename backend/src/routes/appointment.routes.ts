962f61ea5c8b8325f8749f8490f396a4137d8179
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2026-06-08 12:38:52 -0300

    Add appointment routes for managing appointments

---
+++ backend/src/routes/appointment.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { createAppointmentControlller,listAppointmentsController,getAppointmentByIdController,updateAppointmentController,uptadeAppointmentStatusCotroller, deleteAppointmentController } from "../controllers/apponitmentController";
+import { authenticate,authorize } from "../middlewares/authMiddlewares";
+
+const appointmentRouter = Router()
+
+// rotas de agendamenyos todas privadas 
+
+appointmentRouter.use(authenticate)
+
+// rotas privadas - leituras  todos roles podem acessar 
+
+appointmentRouter.get('/',listAppointmentsController)
+appointmentRouter.get('/:id',getAppointmentByIdController)
+
+// rotas privadas - edicao (ADM , SECRETARIA , DENTISTA )
+
+appointmentRouter.post('/',authorize('ADMIN','SECRETARY','DENSTIST'),createAppointmentControlller)
+appointmentRouter.put('/:id',authorize('ADMIN','SECRETARY','DENSTIST'),updateAppointmentController)
+
+// rotas privadas de status 
+
+appointmentRouter.patch('/:id/status', authorize('ADMIN','SECRETARY','DENSTIST'),uptadeAppointmentStatusCotroller)
+
+// rotas privadas de exclusao 
+
+appointmentRouter.delete('/:id',authorize('ADMIN','SECRETARY'),deleteAppointmentController)
+
+export default appointmentRouter
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express'
 import authRoutes from './auth.routes'
 import patientRoutes from './patient.routes'
+import appointmentRouter from './appointment.routes'
 
 const router = Router()
 
 router.use('/auth', authRoutes)
 router.use('/patients', patientRoutes)
+router.use('/appointments', appointmentRouter)
 
 // Novos módulos serão registrados aqui conforme forem criados
 // router.use('/patients', patientRoutes)
-// router.use('/appointments', appointmentRoutes)
+
 // router.use('/products', productRoutes)
 // router.use('/transactions', transactionRoutes)
 
